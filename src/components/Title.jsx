becdca432fc5170857f7ac4b181c2d34cd14f93a
ysantonyance <zinovevakarina569@gmail.com>
2026-07-02 15:46:39 +0300

    Changed my mind

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -11,22 +11,9 @@ function Title() {
   const [count, setCount] = useState(0)
 */
 
-    const buttonStyle = {
-        border: 'rgba(255, 255, 255, 0.04)',
-        borderRadius: '30px',
-        background: 'rgba(255, 255, 255, 0.1)',
-        padding: '16px',
-        margin: '0 16px',
-        color: 'rgba(255, 255, 255, 0.7)',
-        cursor: 'pointer',
-    }
 
   return (
     <>
-        <div className='buttons'>
-            <button>Main Page</button>
-            <button>Concert</button>
-        </div>
       <div className="title-container">
         <div className="artist-picture">
             <img src="https://i.pinimg.com/originals/22/44/02/224402a79cc4464fe0d94902e45a0683.jpg" alt="pfp"/>
